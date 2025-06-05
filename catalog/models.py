0bdc01f171937291e19f6c9acee4b90afdb2965d
Evgeniya <osipova.dr17@yandex.ru>
2025-06-05 23:54:50 +1100

    link with db

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Наименование")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
+
+class Product(models.Model):
+    name_product = models.CharField(max_length=150, verbose_name="Наименование продукта")
+    description = models.TextField(null=True, blank=True, verbose_name="Описание")
+    photo = models.ImageField(upload_to="catalog/image", blank=True, null=True, verbose_name="Изображение продукта")
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="Категории",
+    )
+    price = models.FloatField(max_length=50, verbose_name="Цена за покупку")
+    created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateField(auto_now=True, verbose_name="Дата последнего изменения")
+
+    def __str__(self):
+        return f"'Наименование - {self.name_product}. Цена - {self.price}."
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ["name_product"]
